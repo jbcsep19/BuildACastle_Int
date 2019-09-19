35d77c72dc1334a894999439de09e2b9bfebf404
jnfrle <jennifer.lee1@montgomerycollege.edu>
2019-09-19 15:54:53 -0400

    initial commit

---
+++ src/com/company/Castle.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Castle implements CastleMethods{ // this is our blueprint
+    public Castle(){
+
+    }
+    private String gold;
+    private String magnaCarta;
+    private String princess;
+
+    public String getGold() {
+        return gold;
+    }
+
+    public void setGold(String gold) {
+        this.gold = gold;
+    }
+
+    public String getMagnaCarta() {
+        return magnaCarta;
+    }
+
+    public void setMagnaCarta(String magnaCarta) {
+        this.magnaCarta = magnaCarta;
+    }
+
+    public String getPrincess() {
+        return princess;
+    }
+
+    public void setPrincess(String princess) {
+        this.princess = princess;
+    }
+
+    public void callTheCalvary(){
+        System.out.println("Calling the calvary... HELP!!!");
+
+    }
+
+    public void bringHorse(){
+        System.out.println("I need some horses, pronto");
+
+    };
+
+    public void bringShield(){
+        System.out.println("Time to play defense");
+
+    };
+
+    public void bringMoreWeapon(){
+        System.out.println("MORE SWORDS!");
+
+    };
+
+
+}
